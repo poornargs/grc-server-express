80b8256ff1c09ecc81db7048ffcb422c520bcc20
poornargs <42318980+poornargs@users.noreply.github.com>
2023-02-04 21:06:37 +0530

    Host added

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ app.get('/get/weather/data', (req, res) => {
     })
 })
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Example app listening on port ${port}`)
 })
